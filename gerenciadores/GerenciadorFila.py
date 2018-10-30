37b580f126a533569b2648d01411198ced3601b1
henritar <henriquetorres92@gmail.com>
2018-10-30 17:46:00 -0200

    Commit aletório
    :3

--- gerenciadores/GerenciadorFila.py
+++ gerenciadores/GerenciadorFila.py
@@ -7,15 +7,24 @@ class GerenciadorFila:
 		self.filaProcessosUsuario = [[], [], []]
 
 		for indice in range(len(listaProcessos)):
+
 			if (listaProcessos[indice].prioridadeProcesso == 0):
 				self.filaTempoReal.append(listaProcessos[indice])
+
 			elif (listaProcessos[indice].prioridadeProcesso == 1):
 				self.filaProcessosUsuario[1].append(listaProcessos[indice])
+
 			elif (listaProcessos[indice].prioridadeProcesso == 2):
 				self.filaProcessosUsuario[2].append(listaProcessos[indice])
+
 			elif (listaProcessos[indice].prioridadeProcesso == 3):
 				self.filaProcessosUsuario[3].append(listaProcessos[indice])
+
 			else:
 				print("Prioridade inconsistente")
+
+	# def aging(self):
+
+		
 
 

7e2a2fed6d2d7ec524f75dd9e2346ae23a2af2a2
vitor <vitor.mcr95@gmail.com>
2018-11-02 14:13:20 -0300

    alocando memoria ram para poder executar

--- include/Process.py
+++ include/Process.py
@@ -5,7 +5,7 @@ class Process:
         self.tempoInicializacao = int(listaParametros[0].strip())
         self.prioridadeProcesso = int(listaParametros[1].strip())
         self.tempoProcessador = int(listaParametros[2].strip())
-        self.blocoMemoria  = int(listaParametros[3].strip())
+        self.blocoMemoria = int(listaParametros[3].strip())
         self.codigoImpressora = int(listaParametros[4].strip())
         self.requisicaoScanner = int(listaParametros[5].strip())
         self.requisicaoModem = int(listaParametros[6].strip())

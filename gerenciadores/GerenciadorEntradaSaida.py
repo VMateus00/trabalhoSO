5c2047f0151846a69d124f0afcd96b2b0d87f314
henritar <henriquetorres92@gmail.com>
2018-11-02 14:16:16 -0200

    GerenciadorES
    Dicionario completo.

--- gerenciadores/GerenciadorEntradaSaida.py
+++ gerenciadores/GerenciadorEntradaSaida.py
@@ -4,9 +4,19 @@ import threading
 class GerenciadorEntradaSaida:
 
     def __init__(self):
-        self.scanner = threading.Semaphore(1)
-        self.impressora2 = threading.Semaphore(1)
-        self.impressora1 = threading.Semaphore(1)
-        self.modem = threading.Semaphore(1)
-        self.dispositivosSATA1 = threading.Semaphore(1)
-        self.dispositivosSATA2 = threading.Semaphore(1)
+
+    	self.scanner = threading.Semaphore(1)
+    	self.impressora1 = threading.Semaphore(1)
+    	self.impressora2 = threading.Semaphore(1)
+    	self.modem = threading.Semaphore(1)
+    	self.dispositivosSATA1 = threading.Semaphore(1)
+    	self.dispositivosSATA2 = threading.Semaphore(1)
+
+    	self.dicioES = {
+	        "scanner" : self.scanner,
+	        "impressora1" : self.impressora1,
+	        "impressora2" : self.impressora2,
+	        "modem" : self.modem,
+	        "dispositivosSATA1" : self.dispositivosSATA1,
+	        "dispositivosSATA2" : self.dispositivosSATA2
+		}
